a6511e8be02fa53691934e7cf19b5d5be491c962
Derek <nightwing917@hotmail.com>
2024-01-20 19:51:30 -0700

    added event listener to form

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+
+
+
+// EVENT LISTENERS
+itemForm.addEventListener('submit', addItem);
