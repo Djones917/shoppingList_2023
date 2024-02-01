6dd7e4ddedd750c8bd6c5a0971ac61dae6197ba5
Derek <nightwing917@hotmail.com>
2024-01-31 20:13:41 -0700

    created text node for li

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 
-const newItem = itemInput.value;
+
+
 function addItem(e) {
   e.preventDefault();
 
+  const newItem = itemInput.value;
+
   // Validate
-  if (itemInput.value === '') {
+  if (newItem === '') {
      alert('Add an item');
      return;
   }
-  console.log('success!');
+  
+  // Create List Item
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(newItem));
+
+  console.log(li);
 }
 
 // EVENT LISTENERS
