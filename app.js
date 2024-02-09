91aec9f82cf3c62f4c2c3793a20cb5f15b1d7456
Derek <nightwing917@hotmail.com>
2024-02-08 19:56:08 -0700

    updating app js

--- app.js
+++ app.js
@@ -20,18 +20,23 @@ function addItem(e) {
   li.appendChild(document.createTextNode(newItem));
 
   const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
+  itemList.appendChild(li);
+  itemInput.value = '';
 }
 
 
 function createButton(classes) {
    const button = document.createElement('button');
    button.className = classes;
+   const icon = createIcon('fa-solid fa-xmark');
+   button.appendChild(icon);
    return button;
 }
 
 function createIcon(classes) {
   const icon = document.createElement('i');
-  i.className = classes;
+  icon.className = classes;
   return icon;
 }
 
