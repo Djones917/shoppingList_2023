987da06d95124b04195e7bb43cc850a952ac0f69
Derek <nightwing917@hotmail.com>
2024-02-14 19:56:07 -0700

    grabbed element by id for clear button

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
-
+const itemClear = document.getElementById('clear');
 
 
 function addItem(e) {
