9c77c188d82b404a7fb1575cb70547a489a338ab
Derek <nightwing917@hotmail.com>
2024-02-21 19:46:13 -0700

    added grabbed itemFilter

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 const items = itemList.querySelectorAll('li');
 
 
