7af776d35a7e55f79ab849c2b97ae84471434c60
Derek <nightwing917@hotmail.com>
2024-03-21 19:48:15 -0600

    change addItem event listerner

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ function checkUI() {
 }
 
 // EVENT LISTENERS
-itemForm.addEventListener('submit', addItem);
+itemForm.addEventListener('submit', onaddItemSubmit);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
 itemFilter.addEventListener('input', filterItems);
