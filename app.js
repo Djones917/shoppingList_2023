8fb7965abeef7129efc84ec4acce1118912f57c8
Derek <nightwing917@hotmail.com>
2024-06-12 19:39:01 -0600

    added item form btn

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
+const formBtn = itemForm.querySelector('button');
 let isEditMode = false;
 
 
