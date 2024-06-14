c9441e20811e1ceb7d5fe6a3485407e95cba073e
Derek <nightwing917@hotmail.com>
2024-06-13 19:41:14 -0600

    updating itemInput

--- app.js
+++ app.js
@@ -95,6 +95,7 @@ function setItemToEdit(item) {
   isEditMode = true;
   item.classList.add('edit-mode');
   formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
+  itemInput.value = item.textContent;
 }
 
 
